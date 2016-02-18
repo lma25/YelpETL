66ceff082fc8cbd0accd051d69436661eef9879e
lma25 <lma25@hawk.iit.edu>
2016-02-18 11:50:17 -0600

    Small Modification
    Delete some useless code to make code smaller.

--- src/main/ETLMapper.java
+++ src/main/ETLMapper.java
@@ -15,12 +15,6 @@ public class ETLMapper extends Mapper<Object, Text, Text, InfoWritable>{
 		String[] lines = value.toString().split("\\n");
 		for(String line : lines){
 			Business business = GetInfo.getBusiness(line);
-			//Business business = new Business();
-			//business.city = "Chicago";
-			//business.categories.add("Restaurants");
-			//business.review_count = 5;
-			//business.name = "Hhahaha";
-			//business.stars = 3.2;
 			if(business.review_count > 3 && business.categories.contains("Restaurants")){
 				info.setName(business.name);
 				info.setStars(business.stars);
